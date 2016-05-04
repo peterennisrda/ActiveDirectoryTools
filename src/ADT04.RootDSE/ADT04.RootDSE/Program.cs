e5f04d2d6d7e5141eb08c46d2c22e0f371074de8
peter <peterennis@resourcedevelopment.net>
2016-05-03 18:16:21 -0700

    determine current domain controller (RootDSE)

---
+++ src/ADT04.RootDSE/ADT04.RootDSE/Program.cs
@@ -0,0 +1,38 @@
+﻿#region NameSpaces
+using System;
+using System.DirectoryServices;
+#endregion
+
+namespace ADT04RootDSE
+{
+    class Program
+    {
+        static string theRootDSE = "";
+        // Ref: http://stackoverflow.com/questions/4015407/determine-current-domain-controller-programmatically
+
+        public static string RetrieveRootDseDefaultNamingContext()
+        {
+            String RootDsePath = "LDAP://RootDSE";
+            const string DefaultNamingContextPropertyName = "defaultNamingContext";
+
+            DirectoryEntry rootDse = new DirectoryEntry(RootDsePath)
+            {
+                AuthenticationType = AuthenticationTypes.Secure
+            };
+
+            object propertyValue = rootDse.Properties[DefaultNamingContextPropertyName].Value;
+
+            return propertyValue != null ? propertyValue.ToString() : null;
+        }
+
+        #region Main
+        static void Main(string[] args)
+        {
+            theRootDSE = RetrieveRootDseDefaultNamingContext();
+            Console.WriteLine("The RootDSE is " + theRootDSE);
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
